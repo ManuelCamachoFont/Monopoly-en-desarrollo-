119f0ec89ac65680632a6b0406fc465d6f44c4ab
Leo Ruiz <joselrm91@gmail.com>
2026-05-12 10:08:45 +0200

    Primer commit: Inicio de proyecto y subida de código

--- src/es/Studium/Monopoly/Principal.java
+++ src/es/Studium/Monopoly/Principal.java
@@ -5,7 +5,9 @@ public class Principal
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		MainWindow mainWindow = new MainWindow();
+		Modelo modelo = new Modelo();
+		new Controlador(mainWindow, modelo);
 
 	}
 
---
+++ src/es/Studium/Monopoly/Test.java
@@ -0,0 +1,12 @@
+package es.Studium.Monopoly;
+
+public class Test
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hola mundo");
+		System.out.println("Hello World");
+	}
+
+}
