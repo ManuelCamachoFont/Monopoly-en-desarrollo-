07567b15d491301a9f8841ab73ff852dd6116a9e
Manuel Camacho Font <manuelcamachofont@gmail.com>
2026-06-05 17:01:52 +0200

    fix minor

--- src/es/studium/main/java/TurnManager.java
+++ src/es/studium/main/java/TurnManager.java
@@ -173,6 +173,7 @@ public class TurnManager
 
 	public static boolean buyProperty(Player player, Square square)
 	{
+		//Propiedad
 		if (square == null) return false;
 		if (square.hasOwner()) return false;
 		if (player.getMoney() < square.getPrice()) return false;
