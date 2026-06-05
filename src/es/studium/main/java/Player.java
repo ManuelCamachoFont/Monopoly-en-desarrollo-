8979da24b0eaafe83c76f239ebe73311042d3bd4
Leo Ruiz <joselrm91@gmail.com>
2026-06-05 08:22:43 +0200

    set Class Player setter and getter

--- src/es/studium/main/java/Player.java
+++ src/es/studium/main/java/Player.java
@@ -3,7 +3,8 @@ package es.studium.main.java;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Player {
+public class Player
+{
 
 	private int id;
 	private String name;
@@ -14,101 +15,133 @@ public class Player {
 	private int jailCards = 0;
 	private int doublesDices = 0;
 	private ArrayList<Square> properties;
-	
-	public Player(String name, int moneyStarting) {
-        this.name = name;
-        this.money = moneyStarting;
-        this.position = 1;
-        this.prison = false;
-        this.properties = new ArrayList<>();
-        
-    }
 
-	public int getId() { return id; }
+	public Player(int id, String name, int moneyStarting) {
+		this.id = id;
+		this.name = name;
+		this.money = moneyStarting;
+		this.position = 1;
+		this.prison = false;
+		this.jailTurns = 0;
+		this.jailCards = 0;
+		this.doublesDices = 0;
+		this.properties = new ArrayList<>();
 
-	public void setId(int id) { this.id = id; }
+	}
 
-	public String getName() { return name; }
+	public int getId()
+	{
+		return id;
+	}
 
-	public void setName(String name) { this.name = name; }
+	public void setId(int id)
+	{
+		this.id = id;
+	}
 
-	public int getMoney() {	return money; }
+	public String getName()
+	{
+		return name;
+	}
 
-	public void setMoney(int money) { this.money = money; }
-	
-	public int getPosition() { return position;}
-	
-	public void setPosition(int position) { this.position = position; }
-	
-	public void setPrison(boolean prison) { this.prison = prison; }
-	
-	public boolean getPrison() { return prison; }
-	
-	public ArrayList<Square> getProperties(){ return properties; }
-	
-	
-	public void updateMoney (int updated) {
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public int getMoney()
+	{
+		return money;
+	}
+
+	public void setMoney(int money)
+	{
+		this.money = money;
+	}
+
+	public int getPosition()
+	{
+		return position;
+	}
+
+	public void setPosition(int position)
+	{
+		this.position = position;
+	}
+
+	public void setPrison(boolean prison)
+	{
+		this.prison = prison;
+	}
+
+	public boolean getPrison()
+	{
+		return prison;
+	}
+
+	public ArrayList<Square> getProperties()
+	{
+		return properties;
+	}
+
+	public void updateMoney(int updated)
+	{
 		this.money += updated;
 	}
-	
-	
-	public int getTotalHouses(List<Square> squares) {
-		
+
+	public int getTotalHouses(List<Square> squares)
+	{
+
 		int totalHouses = 0;
-		// Need fix - COMPLETED?
 		for (Square s : this.properties) {
 			Square sq = squares.get(s.getId() - 1);
 			totalHouses += sq.getHouses();
 		}
 		return totalHouses;
 	}
-	
-	public int getTotalHotels(List<Square> squares) {
-	    int totalHotels = 0;
-	    // Need fix - COMPLETED?
-	    for (Square s : this.properties) {
-	        Square sq = squares.get(s.getId() - 1);
-	        if (sq.hasHotel()) {
-	           totalHotels++;
-	        }
-	    }
-	    return totalHotels;
+
+	public int getTotalHotels(List<Square> squares)
+	{
+		int totalHotels = 0;
+		for (Square s : this.properties) {
+			Square sq = squares.get(s.getId() - 1);
+			if (sq.hasHotel()) {
+				totalHotels++;
+			}
+		}
+		return totalHotels;
 	}
 
 	public int getJailTurns()
 	{
-		// TODO Auto-generated method stub
-		return 0;
+		return jailTurns;
 	}
 
 	public void setJailTurns(int turnInJail)
 	{
-		// TODO Auto-generated method stub
-		
+		this.jailTurns = turnInJail;
 	}
 
 	public int getJailCards()
 	{
-		// TODO Auto-generated method stub
-		return 0;
+		return jailCards;
 	}
 
-	public void setJailCards(int i)
+	public void setJailCards(int jailCards)
 	{
-		// TODO Auto-generated method stub
-		
+		this.jailCards = jailCards;
+
 	}
 
 	public int getDoublesDices()
 	{
-		// TODO Auto-generated method stub
-		return 0;
+		return doublesDices;
 	}
 
 	public void setDoublesDices(int anotherDouble)
 	{
-		// TODO Auto-generated method stub
-		
+		this.doublesDices = anotherDouble;
+
 	}
-	
+
 }
